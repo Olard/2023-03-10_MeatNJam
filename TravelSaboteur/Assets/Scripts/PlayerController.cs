054c97cd0ad4558d7b43998adb10e441b311d867
Bernhard Öhlinger <bernhard@oehli.at>
2023-03-10 23:03:11 +0100

    Wall + Player controls

--- TravelSaboteur/Assets/Scripts/PlayerController.cs
+++ TravelSaboteur/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,5 +15,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = Vector3.right * Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical");
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(movement * speed);
     }
 }
