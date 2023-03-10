68655660244235e8d8edccab35d39ec996b8afd4
Bernhard Öhlinger <bernhard@oehli.at>
2023-03-11 00:26:47 +0100

    Added start screen

---
+++ TravelSaboteur/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    private Slider slider;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value += Time.deltaTime;
+    }
+}
