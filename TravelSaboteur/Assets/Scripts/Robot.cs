7ee0f50974a83ae7261c41f018cc8748d6ae04f1
Matthias Holzinger <matthias.holzinger@cloudflight.io>
2023-03-11 22:57:28 +0100

    robot now sees and resets the player

--- TravelSaboteur/Assets/Scripts/Robot.cs
+++ TravelSaboteur/Assets/Scripts/Robot.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Robot : MonoBehaviour
@@ -12,6 +13,11 @@ public class Robot : MonoBehaviour
     public float speed = 4f;
     [SerializeField]
     public float waitTimeAtWaypoint = 1f;
+    [SerializeField]
+    public LayerMask playerLayer;
+    [SerializeField]
+    public LayerMask levelLayer;
+
 
     private Vector3 movementDirection = Vector3.zero;
     private Rigidbody rb;
@@ -58,6 +64,34 @@ public class Robot : MonoBehaviour
         {
             Gizmos.color = new Color(1, 0, 0, 0.5f);
             Gizmos.DrawLine(transform.position, waypoints[currentWaypointIndex].transform.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if player is in collider
+        if (other.GetComponentInParent<PlayerController>() != null)
+        {
+            // check if player can be seen
+            if (Physics.Raycast(
+                transform.position,
+                (other.transform.position - transform.position).normalized,
+                out RaycastHit raycastHit,
+                100f,
+                playerLayer | levelLayer,
+                QueryTriggerInteraction.Ignore
+            ))
+            {
+                if (raycastHit.collider.gameObject.GetComponentInParent<PlayerController>() == null)
+                {
+                    return;
+                }
+
+                // punish player
+                var player = other.GetComponentInParent<PlayerController>();
+                player.transform.position = Vector3.zero;
+                // todo add some effect here and write a message or so
+            }
         }
     }
 }
